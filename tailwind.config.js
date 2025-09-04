2322a01b731c29f57827cb135e7d7ddeff3679d4
ninhnt-xusux <ninhnt.xusux@gmail.com>
2025-09-04 14:51:29 +0700

    add UI section4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,27 @@ module.exports = {
       },
       screens: {
         sp: '390px',
-        '3xl': '1680px'
+        // => @media (min-width: 390px)
+        s2: '412px',
+        // => @media (min-width: 412px)
+        xs: '576px',
+        // => @media (min-width: 576px)
+        sm: '640px',
+        // => @media (min-width: 640px)
+        md: '768px',
+        // => @media (min-width: 768px)
+        tb: '820px',
+        // => @media (min-width: 820px)
+        lg: '1024px',
+        // => @media (min-width: 1024px)
+        xl: '1280px',
+        // => @media (min-width: 1280px)
+        '2xl': '1536px',
+        // => @media (min-width: 1536px)
+        '3xl': '1730px',
+        // => @media (min-width: 1730px)
+        '4xl': '1920px',
+        // => @media (min-width: 1730px)
       },
     },
   },
